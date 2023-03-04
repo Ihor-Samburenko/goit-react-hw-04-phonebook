ff5bcc2b2e2f56e011e9e63b4285c3c2975be405
Ihor-Samburenko <samburenko.igor@gmail.com>
2023-03-04 15:08:26 +0200

    add useeffect

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import ContactsBlock from 'components/ContactsBlock/ContactsBlock';
 import PhoneBookList from 'components/PhonebookList/PhoneBookList';
@@ -8,14 +8,16 @@ import css from '../Phonebook/Phonebook.module.css';
 import { nanoid } from 'nanoid';
 
 const Phonebook = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(() => {
+    const items = JSON.parse(localStorage.getItem('phonebook'));
+    return items?.length ? items : [];
+  });
 
   const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('phonebook', JSON.stringify(contacts));
+  }, [contacts]);
 
   const handleFilterChange = e => setFilter(e.target.value);
 
